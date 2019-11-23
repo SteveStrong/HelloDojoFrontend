8c2c9eb56795d5382569ca89cd9b688a5e87cca0
Strong <Stephen.Strong@unisys.com>
2019-11-22 23:05:58 -0500

    mostly works

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -2,24 +2,29 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Toast, EmitterService } from "../shared/emitter.service";
 
 import { QuestionService } from "./question.service";
+import { AnswerService } from "./answer.service";
+
 import { qQuestion } from '../models';
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
-export class QuestionComponent implements OnInit {
+export class QuestionComponent implements OnInit, OnDestroy {
   sub: any;
-  current: qQuestion;
+  current: qQuestion = new qQuestion({question:'loading'})
 
-  constructor(private qService: QuestionService) {}
+  constructor(
+    private qService: QuestionService,
+    private aService: AnswerService) {
+  }
 
   ngOnInit() {
 
     this.sub = this.qService.getQuestions$().subscribe(filename => {
       this.current = this.qService.firstQuestion()
     })
-    
+
     //EmitterService.registerCommand(this, "RefreshDisplay", this.onRefreshDisplay);
     //EmitterService.processCommands(this);
 
@@ -29,12 +34,26 @@ export class QuestionComponent implements OnInit {
     this.sub && this.sub.unsubscribe();
   }
 
-  doPostTrue(){
-    this.current = this.qService.nextQuestion()
+  doPostTrue() {
+    let data = {
+      id: this.current.Id,
+      answer: true,
+      user: 'user'
+    }
+    //this.aService.postAnswer$(data).subscribe(result =>{
+      this.current = this.qService.nextQuestion()
+    //})
   }
 
-  doPostFalse(){
-    this.current = this.qService.nextQuestion()
+  doPostFalse() {
+    let data = {
+      id: this.current.Id,
+      answer: false,
+      user: 'user'
+    }
+    //this.aService.postAnswer$(data).subscribe(result =>{
+      this.current = this.qService.nextQuestion()
+    //})
   }
 
 }
